8c1568d472f0d4a4c3ed2d7ff2b017a9b4c0d9e0
Paradis2019 <popol.elonda@byupw.edu>
2026-04-14 23:14:16 -0600

    up

--- awaken-client/forge.config.ts
+++ awaken-client/forge.config.ts
@@ -1,12 +1,31 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
+import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    executableName: "awaken-client",
   },
+
+  rebuildConfig: {},
+
+  plugins: [
+    new WebpackPlugin({
+      mainConfig: "./webpack.main.config.ts",
+
+      renderer: {
+        config: "./webpack.renderer.config.ts",
+        entryPoints: [
+          {
+            html: "./src/index.html",
+            js: "./src/renderer.ts",
+            name: "main_window",
+          },
+        ],
+      },
+    }),
+  ],
 
   makers: [
     new MakerSquirrel({
